7f20855dd95903d4cb187e9ba8e4ee0e0cc44ab7
Max <paschedagmax1701@gmail.com>
2026-05-12 11:43:32 +0200

    Delete GUI Projekte directory

--- GUI Projekte/password_generator/app/loadResourcePath.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-def resource_path(relative_path):
-    if hasattr(sys, '_MEIPASS'):
-        base_path = sys._MEIPASS
-    else:
-        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, relative_path)
